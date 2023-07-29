4f569a6d3ceefd4788e9105854aa2cc2bff8eb47
aniketkadu0 <aniketkadu@gmail.com>
2023-07-29 20:42:42 +0530

    again3

--- controller.js
+++ controller.js
@@ -6,21 +6,21 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: cryptr.decrypt(process.env.HOST),
+  user: cryptr.decrypt(process.env.USER),
+  password: cryptr.decrypt(process.env.PASSWORD),
+  database: cryptr.decrypt(process.env.DATABASE),
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("MySQL connected");
+});
+
 exports.addlog = async (req, res, next) => {
-  var mysql = require("mysql");
-
-  var connection = mysql.createConnection({
-    host: cryptr.decrypt(process.env.HOST),
-    user: cryptr.decrypt(process.env.USER),
-    password: cryptr.decrypt(process.env.PASSWORD),
-    database: cryptr.decrypt(process.env.DATABASE),
-  });
-
-  connection.connect((err) => {
-    if (err) throw err;
-    console.log("MySQL connected");
-  });
-
   if (Database.ExecuteSQL(req, connection)) {
     connection.end();
     return res.status(200).send({ message: "Successfully logged" });
